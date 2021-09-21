13e842c7e47663d7b6ca774d5e04fb8a122b0a9e
Paulhennin <henninpaul@gmail.com>
2021-09-21 19:03:31 +0200

    added logo from project already imported

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Iframe from 'react-iframe';
 import './style.scss';
+import PlayrFindR from 'src/assets/playrfindr.png'
+import mangahiro from 'src/assets/mangahiro.PNG'
+import orgatravel from 'src/assets/orgatravel.png'
+import LogoGardien from 'src/assets/LogoGardien.png'
+import TitreGardien from 'src/assets/TitreGardienNoir.png'
 
 export default function Porfolio() {
   return (
@@ -13,6 +18,7 @@ export default function Porfolio() {
         <div className="container__lang-cards react">
           <article className="card react">
             <div className="card-content">
+              <img src={PlayrFindR} alt="logo site PlayrFindr" />
               <h2 className="card-title react">PlayrFindR</h2>
               <p className="card-desc">Site de mise en relation de particulier à particulier pour un instant BoardGaming</p>
               <div className="card-link-deck">
@@ -33,6 +39,7 @@ export default function Porfolio() {
           </article>
           <article className="card react">
             <div className="card-content">
+            <img src={orgatravel} alt="logo site OrgaTravel" className="project_logo"/>
               <h2 className="card-title react">OrgaTravel</h2>
               <p className="card-desc">Une API qui arrivera bientôt, stay tuned !</p>
               <div className="card-link-deck">
@@ -116,6 +123,7 @@ export default function Porfolio() {
         <div className="container__lang-cards ruby">
           <article className="card ruby">
             <div className="card-content">
+            <img src={mangahiro} alt="logo site Mangahiro" className="project_logo"/>
               <h2 className="card-title ruby">Mangahiro</h2>
               <p className="card-desc">Site de critiques Manga url : </p>
               <div className="card-link-deck">
@@ -136,6 +144,10 @@ export default function Porfolio() {
           </article>
           <article className="card ruby">
             <div className="card-content">
+              <div>
+                <img src={LogoGardien} alt="logo site Un Gardien près de chez soi" className="project_logo" />
+                <img src={TitreGardien} alt="titre site Un Gardien près de chez soi" className="project_logo" />
+              </div>
               <h2 className="card-title ruby">Un gardien près de chez soi </h2>
               <p className="card-desc">Site de petSitting</p>
               <div className="card-link-deck">
